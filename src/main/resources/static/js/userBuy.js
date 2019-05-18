74ea35ee7f451e065a58606c94ddd01b7d7421e4
斯文人 <37544325+171250563@users.noreply.github.com>
2019-05-18 22:15:43 +0800

    俊博

--- src/main/resources/static/js/userBuy.js
+++ src/main/resources/static/js/userBuy.js
@@ -1,11 +1,24 @@
 $(document).ready(function () {
     getMovieList();
 
+    function getSchedule(scheduleid) {
+        getRequest(
+            '/schedule/'+ scheduleid,
+            function (res) {
+                return res;
+            },
+            function (error) {
+                alert(error) ;
+            }
+        )
+    }
+
     function getMovieList() {
         getRequest(
             '/ticket/get/' + sessionStorage.getItem('id'),
             function (res) {
-                renderTicketList(res.content);
+                console.log(res.content);
+                // renderTicketList(res.content);
             },
             function (error) {
                 alert(error);
@@ -13,8 +26,24 @@ $(document).ready(function () {
     }
 
     // TODO:填空
-    function renderTicketList(list) {
-
-    }
+    // function renderTicketList(list) {
+    //     if (list.length== 0) {
+    //         $('#date-none-hint').css("display", "");
+    //     } else {
+    //         $('#date-none-hint').css("display", "none");
+    //     }
+    //     var bodyContent="";
+    //
+    //     for (var i=0;i<list.length;i++){
+    //         var scheduleItems=getSchedule(list[i].scheduleId);
+    //         bodyContent += "<tr><td>" + scheduleItems.movieName + "</td>" +
+    //             "<td>" + scheduleItems.hallName + "</td>" +
+    //             "<td>" + list[i].columnIndex+"排"+list[i].rowIndex+"列"+ "</td>" +
+    //             "<td>"+scheduleItems.startTime+"</td>" +
+    //             "<td>"+scheduleItems.endTime+"</tr>"+
+    //             "<td>"+list[i].state+"<td>";
+    //     }
+    //     $('#ticket-list-body').html(bodyContent);
+    // }
 
 });
--- src/main/resources/static/js/userMovieBuy.js
+++ src/main/resources/static/js/userMovieBuy.js
@@ -251,6 +251,7 @@ function payConfirmClick() {
             if ($('#userBuy-cardNum').val() === "123123123" && $('#userBuy-cardPwd').val() === "123123") {
                 postPayRequest();
             } else {
+                cancelTicket();
                 alert("银行卡号或密码错误");
             }
         }
@@ -267,6 +268,24 @@ function postPayRequest() {
     } else {
         url = '/ticket/buy' + param;
     }
+    postRequest(
+        url,
+        null,
+        function (res) {
+            if (res.success) {
+                $('#order-state').css("display", "none");
+                $('#success-state').css("display", "");
+                $('#buyModal').modal('hide');
+            }
+        },
+        function (error) {
+            alert(error);
+        });
+}
+
+function cancelTicket() {
+    var param = '?ticketId=' + order.ticketId.join(',');
+    var url = '/ticket/cancel' + param;
     postRequest(
         url,
         null,
