b70aa0f847331f4dea902378778c96bfeec1d2d0
Hui Jiang <814775538@qq.com>
2019-06-09 15:50:29 +0800

    等待前端测试赠送优惠券

--- src/main/java/com/example/cinema/controller/promotion/CouponController.java
+++ src/main/java/com/example/cinema/controller/promotion/CouponController.java
@@ -26,19 +26,19 @@ public class CouponController {
 
     @GetMapping("/getVIPList")
     public ResponseVO sendCoupon(@RequestParam("target_amount")double target_amount){
-        //TODO: 获取历史消费超过指定金额的会员列表
+        //TODO: 获取历史消费超过指定金额的会员列表，等待前端
         return couponService.getVIPListByAmount(target_amount);
     }
 
     @GetMapping("/getAllCoupon")
     public ResponseVO getAllCoupon(){
-        //TODO: 获取所有种类的优惠券
+        //TODO: 获取所有种类的优惠券，等待前端
         return couponService.getCoupons();
     }
 
     @PostMapping("send")
     public ResponseVO sendCoupon(@RequestBody List<SendCouponForm> sendCouponFormList){
-        //TODO: 根据传入的(用户id：优惠券id)赠送优惠券
+        //TODO: 根据传入的(用户id：优惠券id)赠送优惠券，等待前端
         return couponService.sendCoupon(sendCouponFormList);
     }
 
