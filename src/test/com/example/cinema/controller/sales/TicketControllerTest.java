ecbfe8d488d8edf8cb32868fc2d999d33bdbed6c
Hui Jiang <814775538@qq.com>
2019-06-20 23:56:03 +0800

    第三阶段单元测试完成

--- src/test/com/example/cinema/controller/sales/TicketControllerTest.java
+++ src/test/com/example/cinema/controller/sales/TicketControllerTest.java
@@ -1,8 +1,13 @@
 package com.example.cinema.controller.sales;
 
 import com.example.cinema.CinemaApplicationTest;
+import com.example.cinema.data.sales.TicketMapper;
 import com.example.cinema.po.ChargeRecord;
 import com.example.cinema.po.RefundStrategy;
+import com.example.cinema.po.Ticket;
+import com.example.cinema.vo.RefundForm;
+import com.example.cinema.vo.RefundStrategyFrom;
+import com.example.cinema.vo.RefundStrategyVO;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,67 +22,73 @@ public class TicketControllerTest extends CinemaApplicationTest {
     @Autowired
     private TicketController ticketController;
 
-//    @Test
-//    @Transactional
-//    public void getRefundStrategy() {
-//        try {
-//            List<RefundStrategy> records = (List<RefundStrategy>) vipCardController.getChargeRecord(3).getContent();
-//            StringBuilder sb = new StringBuilder();
-//            for (ChargeRecord c : records) {
-//                sb.append(c.getAmount()).append(":").append(c.getBalance()).append(System.lineSeparator());
-//            }
-//            Assert.assertEquals("300.0:330.0\r\n100.0:430.0\r\n", sb.toString());
-//        }catch (Exception e){
-//            fail();
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    @Test
-//    @Transactional
-//    public void addRefundStrategy() {
-//        try {
-//            List<ChargeRecord> records = (List<ChargeRecord>) vipCardController.getChargeRecord(3).getContent();
-//            StringBuilder sb = new StringBuilder();
-//            for (ChargeRecord c : records) {
-//                sb.append(c.getAmount()).append(":").append(c.getBalance()).append(System.lineSeparator());
-//            }
-//            Assert.assertEquals("300.0:330.0\r\n100.0:430.0\r\n", sb.toString());
-//        }catch (Exception e){
-//            fail();
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    @Test
-//    @Transactional
-//    public void updateRefundStrategy() {
-//        try {
-//            List<ChargeRecord> records = (List<ChargeRecord>) vipCardController.getChargeRecord(3).getContent();
-//            StringBuilder sb = new StringBuilder();
-//            for (ChargeRecord c : records) {
-//                sb.append(c.getAmount()).append(":").append(c.getBalance()).append(System.lineSeparator());
-//            }
-//            Assert.assertEquals("300.0:330.0\r\n100.0:430.0\r\n", sb.toString());
-//        }catch (Exception e){
-//            fail();
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    @Test
-//    @Transactional
-//    public void refundTicket() {
-//        try {
-//            List<ChargeRecord> records = (List<ChargeRecord>) vipCardController.getChargeRecord(3).getContent();
-//            StringBuilder sb = new StringBuilder();
-//            for (ChargeRecord c : records) {
-//                sb.append(c.getAmount()).append(":").append(c.getBalance()).append(System.lineSeparator());
-//            }
-//            Assert.assertEquals("300.0:330.0\r\n100.0:430.0\r\n", sb.toString());
-//        }catch (Exception e){
-//            fail();
-//            e.printStackTrace();
-//        }
-//    }
+    @Autowired
+    private TicketMapper ticketMapper;
+
+    @Test
+    @Transactional
+    public void getRefundStrategy() {
+        try {
+            List<RefundStrategy> strategies = (List<RefundStrategy>) ticketController.getRefundStrategy().getContent();
+            StringBuilder sb = new StringBuilder();
+            for (RefundStrategy r : strategies) {
+                sb.append(r.getHoursBeforeEnd()).append(":").append(r.getRate()).append(System.lineSeparator());
+            }
+            Assert.assertEquals("2:0.5\r\n3:0.75\r\n4:0.9\r\n", sb.toString());
+        }catch (Exception e){
+            fail();
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    @Transactional
+    public void addRefundStrategy() {
+        try {
+            RefundStrategyFrom form = new RefundStrategyFrom();
+            form.setHoursBeforeEnd(5);
+            form.setRate(0.95);
+            RefundStrategyVO strategy = (RefundStrategyVO)ticketController.addRefundStrategy(form).getContent();
+            StringBuilder sb = new StringBuilder();
+            sb.append(strategy.getHoursBeforeEnd()).append(":").append(strategy.getRate());
+            assertTrue(strategy.getId()!=0);
+            Assert.assertEquals("5:0.95", sb.toString());
+        }catch (Exception e){
+            fail();
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    @Transactional
+    public void updateRefundStrategy() {
+        try {
+            RefundStrategyFrom form = new RefundStrategyFrom();
+            form.setId(3);
+            form.setHoursBeforeEnd(5);
+            form.setRate(0.95);
+            RefundStrategyVO strategy = (RefundStrategyVO)ticketController.updateRefundStrategy(form).getContent();
+            StringBuilder sb = new StringBuilder();
+            sb.append(strategy.getHoursBeforeEnd()).append(":").append(strategy.getRate());
+            Assert.assertEquals("5:0.95", sb.toString());
+        }catch (Exception e){
+            fail();
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    @Transactional
+    public void refundTicket() {
+        try {
+            RefundForm form = new RefundForm();
+            form.setUserId(16);
+            form.setTicketId(186);
+            double balance = (double)ticketController.refundTicket(form).getContent();
+            assertEquals(100.0, balance, 0.0);
+        }catch (Exception e){
+            fail();
+            e.printStackTrace();
+        }
+    }
 }
